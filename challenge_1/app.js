c37c1a16f6128bf048d0843f4baade3b1533ab07
Jonathan Mitzman <jonathan.mitzman@gmail.com>
2019-01-28 13:23:48 -0800

    "Minor refactor of code for readability"

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -3,58 +3,54 @@
 var board = [];
 var inputs = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'];
 var currentPlayer = 1; //x
-
+var playIcon = 'X'; //1
 
 /****** CONTROLLER FUNCTIONS & GAME INITIALIZATION ******/
 var initialize = function() {
     console.log("initializing game");
-    let eventListeners = [];
+    board = [['','',''],['','',''],['','','']];
+
+    let eventListeners = []; //remove all previous listeners and generare new listeners
     inputs.forEach(move => {
         eventListeners.push(document.getElementById(move).addEventListener("click", (event) => {
-            //hide this button
-            //pull out the button that was clicked
-            //send the button that was commited to the processMove function
-            let move = event.path[0].outerHTML.match(/id="\w*/)[0].substring(4);
-            //disable the button here
-            let target = document.getElementById(move);
-            target.style.display = "none";
+            let move = event.path[0].outerHTML.match(/id="\w*/)[0].substring(4); //store id tag
+            let checkboxOrigin = document.getElementById(move); //store checkbox element
+            let moveChar = null;
+            if (currentPlayer === 1) moveChar = 'X';
+            else moveChar = 'O';
 
+            checkboxOrigin.style.display = "none"; //hide checkbox
+            document.getElementById((move+'play')).innerHTML = moveChar;
+        //and lastly, send the move to the move processor:
             let moveID = inputs.indexOf(move);
             processMove(moveID); //process move
+        }));
+    });
 
-            //update move here
-
-
-        }))
-    })
-
-    board = [
-             ['','',''],
-             ['','',''],
-             ['','','']
-            ] // empty board
-    currentPlayer = '1';
 }
 
 
 /****** STATE UPDATER (THE GAME BOARD) ******/
 var processMove= (move) => {
-    console.log(`Processing move for ${currentPlayer} at position ${move}`);
-    console.log(`Target: [${~~(move/3)}, ${move%3}]`);
+    //console.log(`Processing move for ${currentPlayer} at position ${move}`);
+    //console.log(`Target: [${~~(move/3)}, ${move%3}]`);
 
     //set move:
     board[~~(move/3)][move%3] = currentPlayer;
 
+    //switch player
     if (currentPlayer === 1) {
         currentPlayer = -1;
-    } else currentPlayer = 1;
+        playIcon = "O";
+    } else {
+        currentPlayer = 1;
+        playIcon = "X";
+    }
 
     //check for victor;
     //if so, disable all buttons and wait for new game
     console.log(`board is now: ${JSON.stringify(board)})`);
-
     processBoard();
-
 }
 
 var processBoard = () => {
