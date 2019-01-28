e4a84f2626484f698264cfa72bba4cdb15906cb8
Jonathan Mitzman <jonathan.mitzman@gmail.com>
2019-01-28 11:44:38 -0800

    "Serialized the event listeners and processed players move string"

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,1 +1,49 @@
-console.log('app loaded');
+// EVENT LISTENERS:
+
+var board = [];
+var inputs = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'];
+var currentPlayer = '';
+
+
+/****** CONTROLLER FUNCTIONS & GAME INITIALIZATION ******/
+var initialize = function() {
+    let eventListeners = [];
+    inputs.forEach(move => {
+        eventListeners.push(document.getElementById(move).addEventListener("click", (event) => {
+            //hide this button
+            //pull out the button that was clicked
+            //send the button that was commited to the processMove function
+            let move = event.path[0].outerHTML.match(/id="\w*/)[0].substring(4);
+
+            //disable the button here
+            document.getElementById(move).disabled = true;
+
+            processMove(move); //process move
+
+            //update move here
+
+        }))
+    })
+
+    board = [
+             ['','',''],
+             ['','',''],
+             ['','','']
+            ] // empty board
+    currentPlayer = 'x';
+}
+
+
+/****** STATE UPDATER (THE GAME BOARD) ******/
+var processMove= (move) => {
+    console.log(`Processing move for ${currentPlayer} at position ${move}`);
+
+    console.log(JSON.stringify(board));
+}
+
+
+
+
+
+//LETS GO!
+initialize();
