dac663b0b6b1880d22b38fba348d63ed18c1fee5
Jonathan Mitzman <jonathan.mitzman@gmail.com>
2019-01-28 12:13:05 -0800

    "Processing move & updating board completed'

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -2,11 +2,12 @@
 
 var board = [];
 var inputs = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'];
-var currentPlayer = '';
+var currentPlayer = 1; //x
 
 
 /****** CONTROLLER FUNCTIONS & GAME INITIALIZATION ******/
 var initialize = function() {
+    console.log("initializing game");
     let eventListeners = [];
     inputs.forEach(move => {
         eventListeners.push(document.getElementById(move).addEventListener("click", (event) => {
@@ -14,13 +15,15 @@ var initialize = function() {
             //pull out the button that was clicked
             //send the button that was commited to the processMove function
             let move = event.path[0].outerHTML.match(/id="\w*/)[0].substring(4);
-
             //disable the button here
-            document.getElementById(move).disabled = true;
+            let target = document.getElementById(move);
+            target.style.display = "none";
 
-            processMove(move); //process move
+            let moveID = inputs.indexOf(move);
+            processMove(moveID); //process move
 
             //update move here
+
 
         }))
     })
@@ -30,18 +33,38 @@ var initialize = function() {
              ['','',''],
              ['','','']
             ] // empty board
-    currentPlayer = 'x';
+    currentPlayer = '1';
 }
 
 
 /****** STATE UPDATER (THE GAME BOARD) ******/
 var processMove= (move) => {
     console.log(`Processing move for ${currentPlayer} at position ${move}`);
+    console.log(`Target: [${~~(move/3)}, ${move%3}]`);
 
-    console.log(JSON.stringify(board));
+    //set move:
+    board[~~(move/3)][move%3] = currentPlayer;
+
+    if (currentPlayer === 1) {
+        currentPlayer = -1;
+    } else currentPlayer = 1;
+
+    //check for victor;
+    //if so, disable all buttons and wait for new game
+    console.log(`board is now: ${JSON.stringify(board)})`);
+
+    processBoard();
+
 }
 
+var processBoard = () => {
+    //check for a win
 
+}
+
+var displayBoard = (move) => {
+    document.getElementById(move).HTML = 'X';
+}
 
 
 
