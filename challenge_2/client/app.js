3c575dd284a4ca9bccbac9f533e19a7634ad0cae
Jonathan Mitzman <jonathan.mitzman@gmail.com>
2019-01-29 11:16:46 -0800

    "Completed basic server communication and skeleton MVC for client"

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,16 @@
+/*
+The client app should be able to submit JSON data to the server,
+receive a response containing a CSV-formatted result (the way this
+is done will vary depending on where you are in the challenge).
+
+Implement all the report generation logic on the server.
+Do not use any external libraries (such as via npm). You
+may use jQuery on the client.
 
 
+
+*/
+
+var state = {}
+var viewer = {}
+var controller = {}
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
 
+app.get('/', (req, res) => {
+    let indexPath = path.join(__dirname, '/client/index.html');
+    res.sendFile(indexPath);
+})
 
+app.listen(port, () => {console.log(`Server listening on ${port}`)});
