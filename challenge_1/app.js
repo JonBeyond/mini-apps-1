50e13bc053dd10a763ebcda0af46973c0626ad88
Jonathan Mitzman <jonathan.mitzman@gmail.com>
2019-01-28 17:20:09 -0800

    "Refactored code (removed comments)"

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -18,7 +18,7 @@
 /******* CONTROLLER / LISTENERS & GAME INITIALIZATION *******/
 
     var initialize = function() {
-        reset();
+        resetState();
 
         document.getElementById('gameBoard').addEventListener("click", (event) => {
             let move = event.path[0].outerHTML.match(/id="\w*/)[0].substring(4);
@@ -32,129 +32,124 @@
             }
         })
 
-        document.getElementById('resetGame').addEventListener("click", () => {reset();});
+        document.getElementById('resetGame').addEventListener("click", () => {resetState();});
     }
 
 /************************ BOARD RESET ************************/
 
-var reset = function() {
-    game.wasTie = false;
-    game.complete = false;
-
-    game.board = [['','',''],['','',''],['','','']];
-    game.moves = [];
-    game.targetIDs.forEach((move) => {document.getElementById(move).innerHTML = '';})
-
-    //swap the start player ************** TODO: this should be done elsewhere
-    if (game.prevWinner === 'X') {
-        game.currentPlayer = -1;
-        game.playIcon = "O";
-    } else { // TODO: this will create a loop of X players if no one ever wins.  Need to fix this later.
-        game.currentPlayer = 1;
-        game.playIcon = "X";
+    var resetState = function() {
+        game.wasTie = false;
+        game.complete = false;
+        game.board = [['','',''],['','',''],['','','']];
+        game.moves = [];
+        game.targetIDs.forEach((move) => {document.getElementById(move).innerHTML = '';})
+        if (game.prevWinner === 'X') { //swap to the loser
+            game.currentPlayer = -1;
+            game.playIcon = "O";
+        } else {
+            game.currentPlayer = 1;
+            game.playIcon = "X";
+        }
+        updateDOM();
     }
-
-    //we've changed the current player, so we need to display that text.
-    updateDOM();
-}
 
 /****************** DOM RENDER ******************/
 
-var updateDOM = function() {
-    //TODO: Update coloring apon a win.  This will also require a reset so it will be labor intensive.
+    var updateDOM = function() {
+        //TODO: Update coloring apon a win.  This will also require a reset so it will be labor intensive.
 
-    //score:
-    document.getElementById('score').innerHTML = `Current Score (X - O - Ties): ${game.score.x} - ${game.score.o} - ${game.score.ties}`;
-    //display whose turn it is:
-    document.getElementById('status').innerHTML = `It is ${game.playIcon}'s turn! Select a position below:`; //*** TODO: DOM RENDER
+        //score:
+        document.getElementById('score').innerHTML = `Current Score (X - O - Ties): ${game.score.x} - ${game.score.o} - ${game.score.ties}`;
+        //display whose turn it is:
+        document.getElementById('status').innerHTML = `It is ${game.playIcon}'s turn! Select a position below:`;
 
-    //display game result:
-    if (game.complete) {
-        document.getElementById('resetGame').disabled = false;
-        if (game.wasTie) {
-            document.getElementById('status').innerHTML = `Tie game! No winners or losers!`;
+        //display game result:
+        if (game.complete) {
+            document.getElementById('resetGame').disabled = false;
+            if (game.wasTie) {
+                document.getElementById('status').innerHTML = `Tie game! No winners or losers!`;
+            } else {
+                document.getElementById('status').innerHTML = `${game.prevWinner} has won!`;
+            }
+        }
+    }
+
+/****************** STATES ******************/
+
+    var placeMove = () => {
+        let lastMove = game.moves.slice(-1)[0];
+        let move = game.targetIDs.indexOf(lastMove);
+        game.board[~~(move/3)][move%3] = game.currentPlayer;
+    }
+
+    var nextPlayer = () => {
+        if (game.currentPlayer === 1) {
+            game.currentPlayer = -1;
+            game.playIcon = "O";
         } else {
-            document.getElementById('status').innerHTML = `${game.prevWinner} has won!`;
+            game.currentPlayer = 1;
+            game.playIcon = "X";
         }
     }
-}
 
-/****************** STATE UPDATER (THE GAME BOARD) ******************/
+    var processBoard = () => {
+        for (let i = 0; i < 3; i++) {
+            let rowSum = 0;
+            for (let j = 0; j < 3; j++) {
+                rowSum += game.board[i][j];
+                if (checkWin(rowSum)) return;
+            }
+        }
+        for (let i = 0; i < 3; i++) {
+            let colSum = 0;
+            for (let j = 0; j < 3; j++) {
+                colSum += game.board[j][i];
+                if (checkWin(colSum)) return;
+            }
+        }
+        let diag1 = game.board[0][0] + game.board[1][1] + game.board[2][2];
+        let diag2 = game.board[0][2] + game.board[1][1] + game.board[2][0];
 
-var placeMove = () => {
-    let lastMove = game.moves.slice(-1)[0];
-    let move = game.targetIDs.indexOf(lastMove);
-    game.board[~~(move/3)][move%3] = game.currentPlayer;
-}
+        if (checkWin(diag1)) return;
+        if (checkWin(diag2)) return;
 
-var nextPlayer = () => {
-    if (game.currentPlayer === 1) {
-        game.currentPlayer = -1;
-        game.playIcon = "O";
-    } else {
-        game.currentPlayer = 1;
-        game.playIcon = "X";
-    }
-}
-
-var processBoard = () => {
-    for (let i = 0; i < 3; i++) {
-        let rowSum = 0;
-        for (let j = 0; j < 3; j++) {
-            rowSum += game.board[i][j];
-            if (checkWin(rowSum)) return;
+        if (game.moves.length === 9) {
+            checkWin(null);
         }
     }
-    for (let i = 0; i < 3; i++) {
-        let colSum = 0;
-        for (let j = 0; j < 3; j++) {
-            colSum += game.board[j][i];
-            if (checkWin(colSum)) return;
+
+    var checkWin = (val) => {
+        if (val === 3) {
+            proccessWin('X');
+            return true;
+        }
+        else if (val === -3) {
+            proccessWin('O');
+            return true;
+        }
+        else if (val === null) {
+            proccessWin(null);
+            return true;
         }
     }
-    let diag1 = game.board[0][0] + game.board[1][1] + game.board[2][2];
-    let diag2 = game.board[0][2] + game.board[1][1] + game.board[2][0];
 
-    if (checkWin(diag1)) return;
-    if (checkWin(diag2)) return;
+    var proccessWin = (winner) => {
+        game.moves = game.moves.concat(game.targetIDs);  //prevent further moves by adding all possible values into the array
 
-    if (game.moves.length === 9) {
-        checkWin(null);
+        if (winner === null) {
+            game.complete = true;
+            game.wasTie = true;
+            game.score.ties++;
+        } else if (winner === 'O') {
+            game.prevWinner = 'O';
+            game.complete = true;
+            game.score.o++;
+        } else if (winner === 'X') {
+            game.prevWinner = 'X';
+            game.complete = true;
+            game.score.x++;
+        }
     }
-}
-
-var checkWin = (val) => {
-    if (val === 3) {
-        proccessWin('X');
-        return true;
-    }
-    else if (val === -3) {
-        proccessWin('O');
-        return true;
-    }
-    else if (val === null) {
-        proccessWin(null);
-        return true;
-    }
-}
-
-var proccessWin = (winner) => {
-    game.moves = game.moves.concat(game.targetIDs);  //prevent further moves by adding all possible values into the array
-
-    if (winner === null) {
-        game.complete = true;
-        game.wasTie = true;
-        game.score.ties++;
-    } else if (winner === 'O') {
-        game.prevWinner = 'O';
-        game.complete = true;
-        game.score.o++;
-    } else if (winner === 'X') {
-        game.prevWinner = 'X';
-        game.complete = true;
-        game.score.x++;
-    }
-}
 
 //LETS GO!
 initialize();
