514fe670e373ae1508ef113c955e4bf1f79cc4ec
Jonathan Mitzman <jonathan.mitzman@gmail.com>
2019-01-30 08:33:50 -0800

    Created basic server (port 3000, no response to any CRUD commands yet)

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,27 @@
+/*
+Use Express to serve up an index.html file and its associated assets
+Build your UI using ReactJS and pre-compile your views using Babel
+Use MongoDB or MySQL to store your user data
+*/
 
+const express = require('express');
+const app = express();
 
+var server = {
+    port: 3000,
+    initialize: () => {
+        router.serveClient();
+        app.listen(server.port, () => {console.log(`Server open, listening on port ${server.port}`)});
+    }
+}
+
+var router = {
+    serveClient: () => {
+        console.log('client serving not available yet');
+        //two lines, one for serving static assets, one serving the HTML file
+    }
+}
+
+var processor = {} //for any server-side processing
+
+server.initialize();
